665bd5fd3f846e66202163aec9126e6047f2846f
Mart Young <mart2great@yahoo.com>
2025-02-09 21:32:24 +0100

    Word Counter: v2

--- wordCounter/wc/main.go
+++ wordCounter/wc/main.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
-	// Calling the count function to count the number of words
-	// recieved from the standard input and printing it out
+	// defining the a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "count Lines")
 
-	fmt.Println(count(os.Stdin))
+	// parsing the flag provided by the user
+	flag.Parse()
+
+	// Calling the count function to count the number of words
+	// or line recieved from the standard input and printing it out
+
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, CountLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
+	// if the countlines flag is not set, we want to count words so we
 	//define the scanner slit type to words (defualt is split by lines)
-	scanner.Split(bufio.ScanWords)
+	if CountLines {
+		scanner.Split(bufio.ScanLines)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	//defining a counter
 	wc := 0
 
 	// for every word scanned, increment the counter
 	for scanner.Scan() {
-		wc++
+		text := scanner.Text()
+		if CountLines {
+			if strings.TrimSpace(text) != "" {
+				wc++
+			}
+		} else {
+			wc++
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
 	}
 	// return the total
 	return wc
